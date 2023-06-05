4e7fc49264447de793fb5bc5d272ddd4bb29bfe3
Elhay Efrat <elhayefrat@gmail.com>
2023-06-05 14:29:01 +0300

    test

--- app/app.py
+++ app/app.py
@@ -13,7 +13,6 @@ st.set_page_config(layout="wide")
 st.title("Call Center")
 
 
-
 st.write("OPENAI RULEZ")
 audio_bytes = audio_recorder(
     text="",
@@ -54,68 +53,64 @@ with st.sidebar:
         runner = custom
         st.success('This is a success message!', icon="✅")
 
-
 if runner:
     selection = st.selectbox("How you want to start using call center :) ?", ["-- select ---", "from file", "from mic", "list existing"])
 
-if selection == "from mic":
-    st.balloons()
-    data_file = f'userdata/call-center-{datetime.now().strftime("%Y%m%d-%H%M%S")}.wav'
-    audio_bytes = audio_recorder(pause_threshold=30)
-    if audio_bytes:
-       with open(data_file, "wb") as fs:
-            fs.write(audio_bytes)
-            
-       st.audio(audio_bytes, format="audio/wav", start_time=0)  
-       if st.button("change to text"):
-           st.write(from_file(data_file,lang))
-       if  st.button("finding key words"):
-           st.write("finding key words ...")
-           st.write(from_file(data_file,lang))
-           st.write(find_keywords(from_file(data_file,lang)),model_name)
+    if selection == "from mic":
+        st.balloons()
+        data_file = f'userdata/call-center-{datetime.now().strftime("%Y%m%d-%H%M%S")}.wav'
+        audio_bytes = audio_recorder(pause_threshold=30)
+        if audio_bytes:
+           with open(data_file, "wb") as fs:
+                fs.write(audio_bytes)
 
-elif selection == "from file":
-    st.snow()
-    audio_bytes1 = st.file_uploader("Upload Files", type=["wav"],accept_multiple_files=False)
-
-    data_file_location = f'uploaded/call-center-{datetime.now().strftime("%Y%m%d-%H%M%S")}.wav'
-
-    if audio_bytes1:
-        # st.write(type(audio_bytes1))
-        audio_bytes2 = audio_bytes1.read()
-        with open(data_file_location, "wb") as fs:
-            fs.write(audio_bytes2)
-        if data_file_location:
-            st.audio(audio_bytes2, format="audio/wav", start_time=0)
-            # if st.button("change to text"):
-            #    st.write(from_file(data_file_location))
-            if st.button("finding key words"):
+           st.audio(audio_bytes, format="audio/wav", start_time=0)  
+           if st.button("change to text"):
+               st.write(from_file(data_file,lang))
+           if  st.button("finding key words"):
                st.write("finding key words ...")
-               st.write(from_file(data_file_location,lang))
-               st.write(find_keywords(from_file(data_file_location,lang)),model_name)
-            if st.checkbox('find spesific something'):
-               something = st.text_input('enter what you would loke to do')
-               lola = from_file(data_file_location,lang)
-               if something:
-                st.write(find_something(lola,something,model_name))
+               st.write(from_file(data_file,lang))
+               st.write(find_keywords(from_file(data_file,lang)),model_name)
 
-elif selection == "list existing":
-    st.snow()
-    selection = st.selectbox("Select file", ['uploaded','userdata'])
-    if selection:
-        selected = st.selectbox("Select file", [f for f in listdir(selection) if isfile(join(selection, f))])
-        if selected:
-            full_path = f'{selection}/{selected}'
-            audio_file = open(full_path, 'rb')
-            audio_bytes = audio_file.read()
-            st.audio(audio_bytes, format="audio/wav", start_time=0)
-            if st.button("change to text"):
-               st.write(from_file(full_path,lang))
-            if st.button("finding key words"):
-               st.write("finding key words ...")
-               st.write(from_file(full_path,lang))
-               st.write(find_keywords(from_file(full_path,lang)),model_name)
-                
-            
+    elif selection == "from file":
+        st.snow()
+        audio_bytes1 = st.file_uploader("Upload Files", type=["wav"],accept_multiple_files=False)
 
+        data_file_location = f'uploaded/call-center-{datetime.now().strftime("%Y%m%d-%H%M%S")}.wav'
+
+        if audio_bytes1:
+            # st.write(type(audio_bytes1))
+            audio_bytes2 = audio_bytes1.read()
+            with open(data_file_location, "wb") as fs:
+                fs.write(audio_bytes2)
+            if data_file_location:
+                st.audio(audio_bytes2, format="audio/wav", start_time=0)
+                # if st.button("change to text"):
+                #    st.write(from_file(data_file_location))
+                if st.button("finding key words"):
+                   st.write("finding key words ...")
+                   st.write(from_file(data_file_location,lang))
+                   st.write(find_keywords(from_file(data_file_location,lang)),model_name)
+                if st.checkbox('find spesific something'):
+                   something = st.text_input('enter what you would loke to do')
+                   lola = from_file(data_file_location,lang)
+                   if something:
+                    st.write(find_something(lola,something,model_name))
+
+    elif selection == "list existing":
+        st.snow()
+        selection = st.selectbox("Select file", ['uploaded','userdata'])
+        if selection:
+            selected = st.selectbox("Select file", [f for f in listdir(selection) if isfile(join(selection, f))])
+            if selected:
+                full_path = f'{selection}/{selected}'
+                audio_file = open(full_path, 'rb')
+                audio_bytes = audio_file.read()
+                st.audio(audio_bytes, format="audio/wav", start_time=0)
+                if st.button("change to text"):
+                   st.write(from_file(full_path,lang))
+                if st.button("finding key words"):
+                   st.write("finding key words ...")
+                   st.write(from_file(full_path,lang))
+                   st.write(find_keywords(from_file(full_path,lang)),model_name)
 
